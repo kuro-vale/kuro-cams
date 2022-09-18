9ad6b876d82187b1a11a1cd10acec2abf25bf5e7
kuro-vale <jsalcedo218@gmail.com>
2022-09-18 01:27:27 +0000

    Update user forms

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,8 +1,6 @@
 // See the Tailwind configuration guide for advanced usage
 // https://tailwindcss.com/docs/configuration
 
-let plugin = require('tailwindcss/plugin')
-
 module.exports = {
   content: [
     './js/**/*.js',
@@ -193,10 +191,6 @@ module.exports = {
     boxShadow: ['focus', 'dark:focus'],
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    plugin(({ addVariant }) => addVariant('phx-no-feedback', ['&.phx-no-feedback', '.phx-no-feedback &'])),
-    plugin(({ addVariant }) => addVariant('phx-click-loading', ['&.phx-click-loading', '.phx-click-loading &'])),
-    plugin(({ addVariant }) => addVariant('phx-submit-loading', ['&.phx-submit-loading', '.phx-submit-loading &'])),
-    plugin(({ addVariant }) => addVariant('phx-change-loading', ['&.phx-change-loading', '.phx-change-loading &']))
+    require('@tailwindcss/forms')
   ]
 }
--- lib/kuro_cams_web/views/error_helpers.ex
+++ lib/kuro_cams_web/views/error_helpers.ex
@@ -11,7 +11,7 @@ defmodule KuroCamsWeb.ErrorHelpers do
   def error_tag(form, field) do
     Enum.map(Keyword.get_values(form.errors, field), fn error ->
       content_tag(:span, translate_error(error),
-        class: "invalid-feedback",
+        class: "text-xs text-red-600 dark:text-red-400",
         phx_feedback_for: input_name(form, field)
       )
     end)
