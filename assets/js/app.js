0e6f722931d8f01e76ccd62af7aab5c80e08e53a
kuro-vale <jsalcedo218@gmail.com>
2022-09-29 17:27:00 +0000

    Add video modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,3 @@
-// We import the CSS which is extracted to its own file by esbuild.
-
-// If you want to use Phoenix channels, run `mix help phx.gen.channel`
-// to get started and then uncomment the line below.
-// import "./user_socket.js"
-
-// You can include dependencies in two ways.
-//
-// The simplest option is to put them in assets/vendor and
-// import them using relative paths:
-//
-//     import "../vendor/some-package.js"
-//
-// Alternatively, you can `npm install some-package --prefix assets` and import
-// them using a path starting with the package name:
-//
-//     import "some-package"
-//
-
 import "./chat_socket.js"
 
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
@@ -24,6 +5,8 @@ import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
 import { Socket } from "phoenix"
 import { LiveSocket } from "phoenix_live_view"
+// Import a simple peer to peer library
+import { Peer } from "peerjs";
 import topbar from "../vendor/topbar"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
@@ -42,3 +25,5 @@ liveSocket.connect()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
+
+window.Peer = Peer
