221912316163dc9ed52845950a11d09f5781e2a4
kuro-vale <jsalcedo218@gmail.com>
2022-09-29 16:14:00 +0000

    Remove unused deps

--- lib/kuro_cams/application.ex
+++ lib/kuro_cams/application.ex
@@ -16,8 +16,7 @@ defmodule KuroCams.Application do
       {Phoenix.PubSub, name: KuroCams.PubSub},
       # Start the Endpoint (http/https)
       KuroCamsWeb.Endpoint,
-      KuroCamsWeb.Presence,
-      KuroCamsWeb.Stun
+      KuroCamsWeb.Presence
       # Start a worker by calling: KuroCams.Worker.start_link(arg)
       # {KuroCams.Worker, arg}
     ]
--- mix.exs
+++ mix.exs
@@ -50,8 +50,7 @@ defmodule KuroCams.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:tailwind, "~> 0.1", runtime: Mix.env() == :dev},
-      {:stun, "~> 1.2"}
+      {:tailwind, "~> 0.1", runtime: Mix.env() == :dev}
     ]
   end
 
